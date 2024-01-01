0580bfcc79113905538ea1a472651378a6cf4254
pandenegeara886 <hawkingtzy@gmail.com>
2024-01-01 20:16:36 +0800

    day1

---
+++ day1.js
@@ -0,0 +1,108 @@
+// alert('hello')
+
+
+
+//prompt('masukan usia')
+
+//var usia = 30 
+// const x =1 //tidak bisa dirubah
+// let y=2
+
+// const usia = 30
+// usia = 25
+
+// console.log('usia adalah '+ usia)
+// alert('usia kamu adalah '+ usia)
+
+// let usia = prompt('berapa usia kamu? ')
+// let nama = 'pande putu puspanegara'
+// let pacar = 2
+// switch (pacar) {
+//     case 1:
+//         pacar = '1 aja'
+//         break;
+//     case 2:
+//         pacar = '2, cukup playboy'
+//         break;
+//     default:
+//         pacar = 'belum punya pacar'
+//         break;
+// }
+// if (pacar == null) {
+//     pacar = 'belum punya'
+    
+// }else{
+//     pacar = 'punya'
+// }
+
+
+
+// let sadloawal = 50000
+// let tambahan = 38000
+
+// const saldoakhir = sadloawal + tambahan
+// alert(`nama saya adalah ${nama} dan umur saya ${usia} pacar saya ${pacar}`)
+// alert(`saldo awalan saya sebesar Rp.${sadloawal} dan saldo tambahan saya sebesar Rp.${tambahan} maka total saldo adalah Rp.${saldoakhir}`)
+
+
+// let namamobil = ['R 35', 'Supra', 'Mileno']
+// namamobil.push('cybertruck')
+// namamobil.shift()
+// namamobil.pop()
+// alert(namamobil)
+
+// let namamotor = []
+// namamotor[0] = 'ZX-25R'
+// alert(namamotor)
+
+// for (let index = 0; index < 5; index++) {
+//     console.log('hello world')
+    
+// }
+
+// const namamotor = ['R1','hayabusa']
+// for (let index = 0; index < namamotor.length; index++) {
+//     console.log(namamotor[index])
+    
+// }
+
+// let i =0
+// // while (i<10) {
+// //     i++
+// //     console.log('pande')
+    
+// // }
+
+// do {
+//     i++
+//     console.log('pande')
+    
+// }while (i<10) 
+
+
+
+
+//==================tugas======================//
+
+
+
+
+//tugas kecil 1
+// let saldoawal = Number(prompt('saldo awal? '))
+// let saldotambahan = Number(prompt('saldo tambahan? '))
+// const saldoakhir = saldoawal+saldotambahan
+// alert(`saldo anda adalah ${saldoakhir}`)
+
+//tugas kecil 2
+// let hari = new Date().getDay()
+
+// let namahari = ['','senin','selasa','rabu','kamis','jumat','sabtu','minggu']
+// for (let index = 0; index < namahari.length; index++) {
+//     if (hari == index) {
+//         hari = namahari[index]
+//     break
+
+//     }
+    
+// }
+// alert(`hari ini adalah hari ${hari}`)
